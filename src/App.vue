d0c473c6b2c0f2f3309ce9daa4463e305aee0238
arafatislam123 <arafatpstu7@gmail.com>
2024-12-22 14:51:27 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+const color='yellow'
+</script>
+
+<template>
+  <button class="text-white font-bold py-2 px-4 rounded" :class="[
+    `bg-${color}-500`,
+    `hover:bg-${color}-700`,
+  ]">
+    Button
+
+  </button>
+
+</template>
+
+<style scoped>
+
+</style>
